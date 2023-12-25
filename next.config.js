ae94eb30f32587ccf5e9eb638a06ab36d025edf2
Rajesh Meena <rajesh.meena1010@gmail.com>
2023-12-25 18:14:53 +0530

    a

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   serverActions: true,
-  //   serverComponentsExternalPackages: [
-  //     "mongoose",
-  //     "twilio",
-  //     "jsonwebtoken",
-  //     "multer",
-  //     "aws-sdk",
-  //   ],
-  // },
+  experimental: {
+    // serverActions: true,
+    // serverComponentsExternalPackages: [
+    //   "mongoose",
+    //   "twilio",
+    //   "jsonwebtoken",
+    //   "multer",
+    //   "aws-sdk",
+    // ],
+  },
 };
 
 module.exports = nextConfig;
